b22017168b7ecddb173b13d222458668bfe68084
Cris8049 <cristopher070404@gmail.com>
2025-06-02 17:09:15 -0400

    Implementación de patrones de diseño

--- blocks/item-list/item-list.js
+++ blocks/item-list/item-list.js
@@ -1,23 +1,36 @@
 import apiItems from "../../services/apiItems.js";
 import "../item-card/item-card.js";
+import ItemStore from "../../services/itemStore.js";
 
 export default class ItemList extends HTMLElement {
     constructor() {
         super();
+        this._store = ItemStore.getInstance();
+        this._onStoreChange = this._onStoreChange.bind(this);
     }
 
     async connectedCallback() {
+        this._store.subscribe(this._onStoreChange);
         const items = await apiItems.getItems();
-        this._render(items);
+        this._items = items;
+        this._render();
     }
 
-    _render(items) {
+    disconnectedCallback() {
+        this._store.unsubscribe(this._onStoreChange);
+    }
+
+    _onStoreChange() {
+        this._render();
+    }
+
+    _render() {
         this.innerHTML = "";
-        if (!items || items.length === 0) {
+        if (!this._items || this._items.length === 0) {
             this.innerHTML = "<p>No hay ítems para mostrar.</p>";
             return;
         }
-        items.forEach((item) => {
+        this._items.forEach((item) => {
             const card = document.createElement("item-card");
             card.dataset.item = JSON.stringify({ id: item.id, name: item.name });
             this.appendChild(card);
